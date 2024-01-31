2a564a67e911dae521816d11a68f06f95c03d58b
nicorubio96 <nicolasrubioolivares96@gmail.com>
2024-01-31 16:52:22 -0300

    ok

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Noto_Sans_Cham } from 'next/font/google'
 import './globals.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import { Analytics } from '@vercel/analytics/react';
 const non = Noto_Sans_Cham({subsets:['latin-ext']})
 
 export const metadata = {
@@ -18,6 +18,7 @@ export default function RootLayout({ children }) {
       
         
         {children}
+        <Analytics />
 
         <Footer/>
 
